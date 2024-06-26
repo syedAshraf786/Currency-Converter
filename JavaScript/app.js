85acdf85229b13b5370b4e946517ed0723e0d5e7
syedAshraf786 <ashrafsyed676@gmail.com>
2024-06-26 07:10:10 +0530

    commit

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,63 +1,59 @@
-let userScore=0;
-let compScore=0;
+const BASE_URL =
+"https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
-const choices = document.querySelectorAll(".choice");
-const msg=document.querySelector("#msg");
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
 
-const userScorePara =document.querySelector("#user-score");
-const compScorePara =document.querySelector("#comp-score");
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
 
-
-const genCompChoice =() => {
-   const options =["rock", "paper", "scissors"];
-   const randIdx = Math.floor(Math.random() * 3);
-   return options[randIdx];;;;
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
 }
 
-const drawGame =()=>{
-    msg.innerText ="Game was Draw.play again.";
-    msg.style.backgroundColor="#081b31";
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data[toCurr.value.toLowerCase()];
+
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
 };
 
-const showWinner=(userWin, userChoice,compChoice) =>{
- if (userWin){
-    userScore++;
-    userScorePara.innerText = userScore;
-
-    msg.innerText =`you win! Your ${userChoice} beats ${compChoice}`;
-    msg.style.backgroundColor="green";
- }else{
-    compScore++;
-    compScorePara.innerText = compScore; 
-    msg.innerText =`you lost. ${compChoice} beats Your ${userChoice}`;
-    msg.style.backgroundColor="red";
- }
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
 };
 
-const playGame = (userChoice) => {
-    console.log("user choice =", userChoice);
-    const compChoice=genCompChoice();
-    console.log("comp choice =", compChoice);
-
-    if(userChoice===compChoice){
-        drawGame();
-    }else{
-        let userWin =true;
-        if (userChoice ==="rock"){
-            userWin=compChoice==="paper" ? false:true;
-        } else if(userChoice==="paper"){
-            userWin = compChoice==="scissors" ? false:true;
-        }else{
-            userWin = compChoice==="rock"? false:true;
-        }
-        showWinner(userWin ,userChoice, compChoice);
-    }
-};
-
-choices.forEach((choice) => {
-    choice.addEventListener("click",() => {
-     const userChoice =choice.getAttribute("id");
-       playGame(userChoice);
-    });
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
 });
 
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
